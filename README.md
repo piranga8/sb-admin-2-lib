4361b757bcfc2a95c28fccdac9a3c55caaea601e
Ignacio Miranda <piranga8@gmail.com>
2020-02-06 04:01:29 -0300

    Configurando package.json para sb-admin-2

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # sb-admin-2-lib
 Include sb-admin-2.css and sb-admin-2.js.
+#### Prerequisites
+  - Node.js
+  - NPM
+  - Git
+
+## Deployment
+In deployment process, you have two commands:
+
+1. Build command
+Used to generate the final result of compiling src files into dist folder. This can be achieved by running the following command:
+```
+> npm start
+```
+
+## Built With
+- [SB Admin 2](https://github.com/BlackrockDigital/startbootstrap-sb-admin-2)
+- [Bootstrap](http://getbootstrap.com/)
+- [Jquery](https://jquery.com/)
+- [Fontawesome](http://fontawesome.io/)
+- [Gulp](https://gulpjs.com/)
+- [Gulp Clean-Css](https://github.com/scniro/gulp-clean-css)
+- [Gulp Header](https://github.com/tracker1/gulp-header)
+- [Gulp Plumber](https://github.com/floatdrop/gulp-plumber)
+- [Gulp Sass](https://github.com/dlmanning/gulp-sass)
+- [Gulp Uglify](https://github.com/terinjokes/gulp-uglify)
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+"use strict";
+
+// Load plugins
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+const gulp = require("gulp");
+const header = require("gulp-header");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+sass.compiler = require('node-sass');
+const uglify = require("gulp-uglify");
+
+// Set the banner content
+const banner = ['/*!\n',
+  ' * Start Bootstrap - SB Admin 2 v4.1.0 (https://startbootstrap.com/template-overviews/sb-admin-2)\n',
+  ' * Copyright 2013-2020 Start Bootstrap\n',
+  ' * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-sb-admin-2/blob/master/LICENSE)\n',
+  ' */\n'
+].join('');
+
+//clean minified versions
+function clean(){
+    return del([
+        "./dist/*"
+    ]);
+}
+
+// Bring third party dependencies from node_modules into vendor directory
+function modules() {
+  var bootstrapSCSS = gulp.src('./node_modules/bootstrap/scss/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap/scss'));
+  return bootstrapSCSS;
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src("./scss/*.scss")
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: "expanded",
+      includePaths: "./node_modules",
+    }))
+    .on("error", sass.logError)
+    .pipe(header(banner))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("dist/css"));
+}
+
+// JS task
+function js() {
+  return gulp
+    .src('./js/sb-admin-2.js')
+    .pipe(header(banner))
+    .pipe(gulp.dest(".dist/js"))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/js'));
+}
+
+// Define complex tasks
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor, gulp.parallel(css, js));
+
+// Export tasks
+exports.clean = clean;
+exports.build = build;
+exports.default = build;
